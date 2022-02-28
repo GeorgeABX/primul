f5baa33e316ce5acabb53e2444c3d95d7c4b514d
GeorgeABX <cirliggeorge72@gmail.com>
2022-02-28 23:17:36 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,8 +5,16 @@ public class Main {
     public static void main(String[] args) throws IOException {
         File file = new File("D:\\Facultate\\PP\\lab1\\lab1_pb2\\exemplu.txt");
         BufferedReader br = new BufferedReader(new FileReader(file));
-        String s;
-        while ((s = br.readLine()) != null)
+        String s,fin=new String();
+        while ((s = br.readLine()) != null){
             System.out.println(s);
+            fin+=s;
+        }
+        int i;
+        StringBuilder str=new StringBuilder(fin);
+        for (i=0;i< fin.length();i++)
+            if(str.charAt(i)==',' ||str.charAt(i)=='.'||str.charAt(i)=='!'||str.charAt(i)=='?')
+                str.setCharAt(i,' ');
+        System.out.println(str);
     }
 }
