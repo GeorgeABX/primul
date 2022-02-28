0562192a87d2eb2098a66ab91878b7b98802ec35
GeorgeABX <cirliggeorge72@gmail.com>
2022-02-28 23:31:14 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -16,5 +16,15 @@ public class Main {
             if(str.charAt(i)==',' ||str.charAt(i)=='.'||str.charAt(i)=='!'||str.charAt(i)=='?')
                 str.setCharAt(i,' ');
         System.out.println(str);
+
+        StringBuilder str2=new StringBuilder(str);
+        for (i=1;i< str2.length();i++)
+        {
+            if(str2.charAt(i-1)==' ' && str2.charAt(i)==' ') {
+                str2.deleteCharAt(i);
+                i--;
+            }
+        }
+        System.out.println(str2);
     }
 }
